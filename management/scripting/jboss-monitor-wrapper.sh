b921add3e5ab5632e52b7b4add89a0b7580c7ee3
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2014-12-09 14:54:37 +0100

    Fix search for groovy binary.
    Don’t redirect stdout to null.

--- management/scripting/jboss-monitor-wrapper.sh
+++ management/scripting/jboss-monitor-wrapper.sh
@@ -2,7 +2,7 @@
 # bash wrapper script for JBoss 7 Groovy-based Nagios plugin
 JBOSS_CLIENT=/opt/jboss-client/
 
-if [ ! -z `which groovy > /dev/null 2>&1` ] ; then
+if [ ! -z `which groovy 2> /dev/null` ] ; then
     GROOVY=`which groovy`
 elif [ -x "$GROOVY_HOME/bin/groovy" ] ; then
     GROOVY=$GROOVY_HOME/bin/groovy
@@ -10,6 +10,7 @@ elif [ -x "/opt/groovy/bin/groovy" ] ; then
     GROOVY=/opt/groovy/bin/groovy
 else
     echo Include groovy in PATH or set GROOVY_HOME or install groovy in /opt/groovy.
+    exit 2
 fi
 
 CLASSPATH=.:`dirname $0`
